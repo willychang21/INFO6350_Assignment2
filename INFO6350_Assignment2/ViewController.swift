9a2c5df342772f2d47059b7c6f3800892bd58df8
willychang21 <willychang17@gmail.com>
2023-02-11 16:46:27 -0800

    Initial Commit

---
+++ INFO6350_Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INFO6350_Assignment2
+//
+//  Created by 張竣傑 on 2/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
